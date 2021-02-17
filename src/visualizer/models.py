c5babddea757d47de5fdb1369fc75d5cdf6ced37
Michael <mpgonz@gmail.com>
2021-02-17 16:00:28 -0500

    defined models for albums and rendered visuals

--- src/visualizer/models.py
+++ src/visualizer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Album(models.Model):
+    uri = models.CharField(max_length=100)
+    artist_name = models.CharField(max_length=100)
+    album_name = models.CharField(max_length=100)
+    raw_image = models.ImageField(upload_to='raw')
+
+
+class Render(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    render_mode = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='rendered')
