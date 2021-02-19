91663df8686c9d2c343639cd13925605829ddd24
Michael <mpgonz@gmail.com>
2021-02-19 18:20:12 -0500

    general model works, but just passes around json instead of rendered images

--- src/api/views.py
+++ src/api/views.py
@@ -1,5 +1,6 @@
 from .albumvis import Visualizer
 from django.shortcuts import render
+from django.http import JsonResponse
 
 
 def index(request):
@@ -10,14 +11,14 @@ def index(request):
 
 def playing(request, username):
     track = Visualizer(username).currently_playing_track()
-    track_name = 'no track currently playing' if track is None else track['item'***REMOVED***['name'***REMOVED***
-    context = {'track': track_name, 'js': {'path': 'uhoh.jpg'}}
-    return render(request, 'api/index.html', context)
+    track_obj = {
+        'error': 'no track currently playing'} if track is None else track
+    return JsonResponse(track_obj)
 
 
 def playingnext(request, username):
     vis = Visualizer(username)
     track = vis.wait_for_next_track()
-    track_name = 'no track currently playing' if track is None else track['item'***REMOVED***['name'***REMOVED***
-    context = {'track': track_name, 'js': {'path': 'uhoh.jpg'}}
-    return render(request, 'api/index.html', context)
+    track_obj = {
+        'error': 'no track currently playing'} if track is None else track
+    return JsonResponse(track_obj)
--- src/visualizer/static/polling.js
+++ src/visualizer/static/polling.js
@@ -1,5 +1,8 @@
-function updateimage() {
-  document.getElementById("vis").src = "static/uhoh.jpg";
+function updateimage(username) {
+  $.get("/api/playing/" + username, function(data, status) {
+    console.log("Data: " + data + "\nStatus: " + status);
+  });
+  document.getElementById("vis").src = "/static/uhoh.jpg";
 }
 
 function reqloop() {
