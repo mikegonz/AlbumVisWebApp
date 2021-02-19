4b3ed7f71fdc264286de0d89f64ebdb1cd71a1e1
Michael <mpgonz@gmail.com>
2021-02-19 17:31:42 -0500

    added code for api app

--- src/api/albumvis.py
+++ src/api/albumvis.py
@@ -1,5 +1,6 @@
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
+from time import sleep
 
 
 class Visualizer:
@@ -12,3 +13,15 @@ class Visualizer:
         track = self.sp.current_user_playing_track()
         if track is not None:
             return track
+
+    # TODO: if track is None it should wait until newtrack is not None,
+    # also it shouldn't return if track is different but from same album
+    def wait_for_next_track(self):
+        track = self.sp.current_user_playing_track()
+        while(track):
+            sleep(3)
+            newtrack = self.sp.current_user_playing_track()
+            if track['item'***REMOVED***['id'***REMOVED*** != newtrack['item'***REMOVED***['id'***REMOVED***:
+                return newtrack
+        if track is None:
+            return None
--- src/api/urls.py
+++ src/api/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('playing/<str:username>/', views.playing, name='playing')
+    path('playing/<str:username>/', views.playing, name='playing'),
+    path('playingnext/<str:username>/', views.playingnext, name='playingnext')
 ***REMOVED***
